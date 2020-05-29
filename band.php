61fe273603f779ba7d9f0bb1f11582da9b2eeb9b
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-05-29 09:16:52 +0200

    af

--- band.php
+++ band.php
@@ -5,16 +5,14 @@ $numSum = 0;
 $albumoverzicht = array('citizen of glass' => 4.50,
 						'night' => 9,
 						'new eyes' => 5,
-						'strange trails' => 10);
+						'strange trails' => 20);
 
 foreach ($albumoverzicht as $album => $prijs) {
 	echo $album . ' kost ' . $prijs;
-	echo PHP_EOL;
-	$numCount++;
-	$numSum += $prijs;
+	echo PHP_EOL;																																																																							
 };
 
-$numTotal = $numSum / count($albumoverzicht);
+$numTotal = array_sum($albumoverzicht) / count($albumoverzicht);
 
 echo "je totale prijs is ". array_sum($albumoverzicht) . PHP_EOL;
 echo "je gemildelde prijs is " . $numTotal; 
